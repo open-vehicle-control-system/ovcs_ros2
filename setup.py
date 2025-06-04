a2271525247404b5f8dd4257a3495a6e5de46356
Loic Vigneron <loic.vigneron@gmail.com>
2025-06-04 18:27:41 +0200

    Add resource back

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'ovcs_ros2'
+package_name = 'ovcs'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        # ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (
             os.path.join('share', package_name, 'launch'),
@@ -32,10 +32,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'stereo_camera_node = ovcs_ros2.stereo_camera_publisher:main',
+            'stereo_camera_node = ovcs.stereo_camera_publisher:main',
         ],
         'ros2_components': [
-            'StereoCameraPublisher = ovcs_ros2.stereo_camera_publisher:StereoCameraPublisher',
+            'StereoCameraPublisher = ovcs.stereo_camera_publisher:StereoCameraPublisher',
         ],
     },
 )
